2b24ca186a0a4d143f7f8eee29543b1f95974157
Matěj Hanke <hanke.matej@seznam.cz>
2026-04-26 23:06:34 +0200

    idk

--- CodeGenerator.py
+++ CodeGenerator.py
@@ -1,13 +1,12 @@
 from PLC_ProjectVisitor import PLC_ProjectVisitor
 from PLC_ProjectParser import PLC_ProjectParser
 
-# Generátor kódu - vytváří instrukce pro zásobníkový stroj (VM)
 class CodeGenerator(PLC_ProjectVisitor):
     def __init__(self, symbol_table, node_types):
-        self.symbol_table = symbol_table # Tabulka symbolů z TypeCheckeru
-        self.node_types = node_types     # Typy uzlů z TypeCheckeru
-        self.instructions = []           # Seznam vygenerovaných instrukcí
-        self.label_count = 0             # Počítadlo pro unikátní návěstí
+        self.symbol_table = symbol_table # Symbol table from typechecker: variable_name -> type (I, F, B, S)
+        self.node_types = node_types     # Data types of each expression
+        self.instructions = []           # Instructions
+        self.label_count = 0             # Counter for label (so ewach is unique)
 
     def new_label(self):
         self.label_count += 1
@@ -17,7 +16,7 @@ class CodeGenerator(PLC_ProjectVisitor):
         self.instructions.append(instr)
 
     def visitProgram(self, ctx):
-        # Inicializace proměnných na výchozí hodnoty (podle zadání: 0, 0.0, "", false)
+        # Init of variables to defaults (0, 0.0, "", false)
         for name, var_type in self.symbol_table.items():
             if var_type == 'I': self.add_instr("push I 0")
             elif var_type == 'F': self.add_instr("push F 0.0")
@@ -29,53 +28,63 @@ class CodeGenerator(PLC_ProjectVisitor):
             self.visit(stmt)
         return None
 
-    # Příkaz výraz: např. a = 5; (hodnota výrazu se na konci zahodí pomocí pop)
+    # lonely expression :D (a = 5;) (value gets poped from stack at the end we dont need that)
     def visitExpressionStatement(self, ctx):
         self.visit(ctx.expression())
         self.add_instr("pop")
         return None
 
+    # Literal expression means we have to push that onto stack
     def visitLiteralExpr(self, ctx):
         t = self.node_types[ctx]
         val = ctx.literal().getText()
         self.add_instr(f"push {t} {val}")
         return t
 
+    # Calling identifier has to load value from that variable to the stack 
     def visitIdentifierExpr(self, ctx):
         name = ctx.ID().getText()
         self.add_instr(f"load {name}")
         return self.node_types[ctx]
 
+    # Parenthesis .. just go inside
     def visitParenthesisExpr(self, ctx):
         return self.visit(ctx.expression())
 
+    # Unary minus negetes value
     def visitUnaryMinusExpr(self, ctx):
         t = self.visit(ctx.expression())
         self.add_instr(f"uminus {t}")
         return t
 
+    # NOT
     def visitLogicalNotExpr(self, ctx):
         self.visit(ctx.expression())
         self.add_instr("not")
         return 'B'
 
-    # Pomocná metoda pro návštěvu výrazu a případné přetypování na požadovaný typ
+    # Auto cast
     def visit_and_cast(self, expr_ctx, target_type):
         actual_type = self.visit(expr_ctx)
         if target_type == 'F' and actual_type == 'I':
-            self.add_instr("itof") # Převede int na float na vrcholu zásobníku
+            self.add_instr("itof") # Converts int at the top of the stack to float
         return target_type
 
+    # * / %
     def visitMultiplicativeExpr(self, ctx):
         res_type = self.node_types[ctx]
         op = ctx.op.text
         self.visit_and_cast(ctx.expression(0), res_type)
         self.visit_and_cast(ctx.expression(1), res_type)
-        if op == '*': self.add_instr(f"mul {res_type}")
-        elif op == '/': self.add_instr(f"div {res_type}")
-        elif op == '%': self.add_instr("mod")
+        if op == '*': 
+            self.add_instr(f"mul {res_type}")
+        elif op == '/': 
+            self.add_instr(f"div {res_type}")
+        elif op == '%': 
+            self.add_instr("mod")
         return res_type
 
+    # + - .
     def visitAdditiveExpr(self, ctx):
         res_type = self.node_types[ctx]
         op = ctx.op.text
@@ -86,22 +95,28 @@ class CodeGenerator(PLC_ProjectVisitor):
             return 'S'
         self.visit_and_cast(ctx.expression(0), res_type)
         self.visit_and_cast(ctx.expression(1), res_type)
-        if op == '+': self.add_instr(f"add {res_type}")
-        elif op == '-': self.add_instr(f"sub {res_type}")
+        if op == '+': 
+            self.add_instr(f"add {res_type}")
+        elif op == '-': 
+            self.add_instr(f"sub {res_type}")
         return res_type
 
+    # < >
     def visitRelationalExpr(self, ctx):
         op = ctx.op.text
-        # Pro porovnání určíme společný typ (pokud je jeden float, oba budou float)
+        # Common type is F if either of them is F
         l_type = self.node_types[ctx.expression(0)]
         r_type = self.node_types[ctx.expression(1)]
         common_type = 'F' if (l_type == 'F' or r_type == 'F') else 'I'
         self.visit_and_cast(ctx.expression(0), common_type)
         self.visit_and_cast(ctx.expression(1), common_type)
-        if op == '>': self.add_instr(f"gt {common_type}")
-        elif op == '<': self.add_instr(f"lt {common_type}")
+        if op == '>': 
+            self.add_instr(f"gt {common_type}")
+        elif op == '<': 
+            self.add_instr(f"lt {common_type}")
         return 'B'
 
+    # == !=
     def visitEqualityExpr(self, ctx):
         op = ctx.op.text
         l_type = self.node_types[ctx.expression(0)]
@@ -114,29 +129,34 @@ class CodeGenerator(PLC_ProjectVisitor):
         self.visit_and_cast(ctx.expression(0), common_type)
         self.visit_and_cast(ctx.expression(1), common_type)
         self.add_instr(f"eq {common_type}")
-        if op == '!=': self.add_instr("not")
+        if op == '!=': 
+            self.add_instr("not")   # I just negate the equality result 
         return 'B'
 
+    # AND
     def visitLogicalAndExpr(self, ctx):
         self.visit(ctx.expression(0))
         self.visit(ctx.expression(1))
         self.add_instr("and")
         return 'B'
 
+    # OR
     def visitLogicalOrExpr(self, ctx):
         self.visit(ctx.expression(0))
         self.visit(ctx.expression(1))
         self.add_instr("or")
         return 'B'
 
+    # = 
     def visitAssignmentExpr(self, ctx):
         name = ctx.ID().getText()
         var_type = self.symbol_table[name]
         self.visit_and_cast(ctx.expression(), var_type)
-        self.add_instr(f"save {name}") # Uloží hodnotu do proměnné
-        self.add_instr(f"load {name}") # Přiřazení vrací hodnotu (pro a = b = 5)
+        self.add_instr(f"save {name}") # Saves value to the variable from top of the stack
+        self.add_instr(f"load {name}") # I have to load that value back to the stack in case of a = b = 5
         return var_type
 
+    # write
     def visitWriteStatement(self, ctx):
         exprs = ctx.exprList().expression()
         for expr in exprs:
@@ -144,25 +164,28 @@ class CodeGenerator(PLC_ProjectVisitor):
         self.add_instr(f"print {len(exprs)}")
         return None
 
+    # read
     def visitReadStatement(self, ctx):
         for id_node in ctx.idList().ID():
             name = id_node.getText()
             var_type = self.symbol_table[name]
-            self.add_instr(f"read {var_type}")
-            self.add_instr(f"save {name}")
+            self.add_instr(f"read {var_type}")  # reads input of specified type
+            self.add_instr(f"save {name}")      # saves it into the variable 
         return None
 
+    # just statement parenthesis, we go inside
     def visitBlockStatement(self, ctx):
         for stmt in ctx.statement():
             self.visit(stmt)
         return None
 
+    # if
     def visitIfStatement(self, ctx):
         l_else = self.new_label()
         l_end = self.new_label()
         
         self.visit(ctx.expression())
-        self.add_instr(f"fjmp {l_else}") # Skoč na else, pokud je podmínka false
+        self.add_instr(f"fjmp {l_else}") # Jump to else if condition is false
         self.visit(ctx.statement(0))
         self.add_instr(f"jmp {l_end}")
         self.add_instr(f"label {l_else}")
@@ -175,10 +198,10 @@ class CodeGenerator(PLC_ProjectVisitor):
         l_start = self.new_label()
         l_end = self.new_label()
         
-        self.add_instr(f"label {l_start}")
+        self.add_instr(f"label {l_start}")  # Start
         self.visit(ctx.expression())
-        self.add_instr(f"fjmp {l_end}") # Skoč na konec, pokud je podmínka false
+        self.add_instr(f"fjmp {l_end}")     # Jump to the end if condition is false
         self.visit(ctx.statement())
         self.add_instr(f"jmp {l_start}")
-        self.add_instr(f"label {l_end}")
-        return None
+        self.add_instr(f"label {l_end}")    # End
+        return None 
--- TypeChecker.py
+++ TypeChecker.py
@@ -1,19 +1,23 @@
 from PLC_ProjectVisitor import PLC_ProjectVisitor
 from PLC_ProjectParser import PLC_ProjectParser
 
-# Sémantický analyzátor - kontroluje datové typy a deklarace proměnných
 class TypeChecker(PLC_ProjectVisitor):
     def __init__(self):
-        self.symbol_table = {} # Tabulka symbolů: název_proměnné -> typ (I, F, B, S)
-        self.node_types = {}   # Pomocná tabulka pro uložení výsledného typu každého výrazu (pro generátor)
-        self.errors = []
+        self.symbol_table = {}  # Symbol table: variable_name -> type (I, F, B, S)
+        self.node_types = {}    # Table for result data types of each expression (for generator)
+        self.errors = []       
 
     def report_error(self, ctx, message):
-        line = ctx.start.line
-        column = ctx.start.column
+        line, column = 0, 0
+        if hasattr(ctx, 'start') and hasattr(ctx.start, 'line'):
+            line = ctx.start.line
+            column = ctx.start.column
+        elif hasattr(ctx, 'line'):
+            line = ctx.line
+            column = ctx.column
         self.errors.append(f"{line}:{column} - {message}")
 
-    # Deklarace proměnných: např. int a, b;
+    # Variable declaration: (int a, b;)
     def visitDeclarationStatement(self, ctx):
         var_type_str = ctx.type_().getText()
         type_map = {'int': 'I', 'float': 'F', 'bool': 'B', 'string': 'S'}
@@ -26,7 +30,7 @@ class TypeChecker(PLC_ProjectVisitor):
                 self.symbol_table[name] = var_type
         return None
 
-    # Použití proměnné ve výrazu
+    # Variable check
     def visitIdentifierExpr(self, ctx):
         name = ctx.ID().getText()
         if name not in self.symbol_table:
@@ -37,7 +41,7 @@ class TypeChecker(PLC_ProjectVisitor):
         self.node_types[ctx] = t
         return t
 
-    # Literály (čísla, true/false, řetězce)
+    # Literals check (numbers, true/false, strings)
     def visitLiteralExpr(self, ctx):
         lit = ctx.literal()
         if lit.INT(): t = 'I'
@@ -48,11 +52,13 @@ class TypeChecker(PLC_ProjectVisitor):
         self.node_types[ctx] = t
         return t
 
+    # Parentehesis ckeck (just goes recursively inside and returns its result type)
     def visitParenthesisExpr(self, ctx):
         t = self.visit(ctx.expression())
         self.node_types[ctx] = t
         return t
 
+    # Unary minus (works just for numbers ofc)
     def visitUnaryMinusExpr(self, ctx):
         t = self.visit(ctx.expression())
         if t not in ['I', 'F']:
@@ -61,6 +67,7 @@ class TypeChecker(PLC_ProjectVisitor):
         self.node_types[ctx] = t
         return t
 
+    # NOT
     def visitLogicalNotExpr(self, ctx):
         t = self.visit(ctx.expression())
         if t != 'B':
@@ -69,72 +76,79 @@ class TypeChecker(PLC_ProjectVisitor):
         self.node_types[ctx] = 'B'
         return 'B'
 
-    # Pomocná funkce pro určení typu výsledku binární operace
+    # Helper for resolution data type of the binary operation (+, -, *, ==, ...) result
     def get_binary_type(self, left, right, op, ctx):
-        if left == 'ERROR' or right == 'ERROR': return 'ERROR'
+        if left == 'ERROR' or right == 'ERROR': 
+            return 'ERROR'
         
-        # Aritmetika: pokud je jeden float, výsledek je float
+        # If there is one float result is float
         if op in ['+', '-', '*', '/']:
             if left in ['I', 'F'] and right in ['I', 'F']:
                 return 'F' if (left == 'F' or right == 'F') else 'I'
+            
             self.report_error(ctx, f"Operátor '{op}' vyžaduje číselné typy.")
             return 'ERROR'
         
-        # Modulo pouze pro int
+        # Modulo is for int only
         if op == '%':
             if left == 'I' and right == 'I': return 'I'
             self.report_error(ctx, "Modulo '%' vyžaduje 'int'.")
             return 'ERROR'
             
-        # Spojování řetězců
+        # Concat
         if op == '.':
             if left == 'S' and right == 'S': return 'S'
             self.report_error(ctx, "Konkatenace '.' vyžaduje 'string'.")
             return 'ERROR'
             
-        # Relace (<, >)
+        # Comparison
         if op in ['<', '>']:
             if left in ['I', 'F'] and right in ['I', 'F']: return 'B'
             self.report_error(ctx, f"Porovnání '{op}' vyžaduje číselné typy.")
             return 'ERROR'
             
-        # Rovnost (==, !=)
+        # Equality
         if op in ['==', '!=']:
             if left == right and left in ['I', 'F', 'S', 'B']: return 'B'
-            if left in ['I', 'F'] and right in ['I', 'F']: return 'B' # Porovnání int a float je ok
+            if left in ['I', 'F'] and right in ['I', 'F']: return 'B'
             self.report_error(ctx, f"Porovnání '{op}' vyžaduje kompatibilní typy.")
             return 'ERROR'
             
         return 'ERROR'
 
+    # * / %
     def visitMultiplicativeExpr(self, ctx):
         l = self.visit(ctx.expression(0))
         r = self.visit(ctx.expression(1))
         t = self.get_binary_type(l, r, ctx.op.text, ctx)
         self.node_types[ctx] = t
         return t
-
+    
+    # + - .
     def visitAdditiveExpr(self, ctx):
         l = self.visit(ctx.expression(0))
         r = self.visit(ctx.expression(1))
         t = self.get_binary_type(l, r, ctx.op.text, ctx)
         self.node_types[ctx] = t
         return t
-
+    
+    # < >
     def visitRelationalExpr(self, ctx):
         l = self.visit(ctx.expression(0))
         r = self.visit(ctx.expression(1))
         t = self.get_binary_type(l, r, ctx.op.text, ctx)
         self.node_types[ctx] = t
         return t
-
+    
+    # == !=
     def visitEqualityExpr(self, ctx):
         l = self.visit(ctx.expression(0))
         r = self.visit(ctx.expression(1))
         t = self.get_binary_type(l, r, ctx.op.text, ctx)
         self.node_types[ctx] = t
         return t
-
+    
+    # &&
     def visitLogicalAndExpr(self, ctx):
         l = self.visit(ctx.expression(0))
         r = self.visit(ctx.expression(1))
@@ -144,7 +158,8 @@ class TypeChecker(PLC_ProjectVisitor):
             t = 'ERROR'
         self.node_types[ctx] = t
         return t
-
+    
+    # ||
     def visitLogicalOrExpr(self, ctx):
         l = self.visit(ctx.expression(0))
         r = self.visit(ctx.expression(1))
@@ -155,7 +170,7 @@ class TypeChecker(PLC_ProjectVisitor):
         self.node_types[ctx] = t
         return t
 
-    # Přiřazení: a = výraz
+    # Assignment (a = 5;)
     def visitAssignmentExpr(self, ctx):
         name = ctx.ID().getText()
         if name not in self.symbol_table:
@@ -170,12 +185,12 @@ class TypeChecker(PLC_ProjectVisitor):
             self.node_types[ctx] = 'ERROR'
             return 'ERROR'
         
-        # Kontrola kompatibility typů při přiřazení
+        # data type compatibility check
         if var_type == expr_type:
             self.node_types[ctx] = var_type
             return var_type
         if var_type == 'F' and expr_type == 'I':
-            # Automatické přetypování int -> float je povoleno
+            # Auto casting
             self.node_types[ctx] = 'F'
             return 'F'
         
@@ -183,6 +198,7 @@ class TypeChecker(PLC_ProjectVisitor):
         self.node_types[ctx] = 'ERROR'
         return 'ERROR'
 
+    # if
     def visitIfStatement(self, ctx):
         cond_type = self.visit(ctx.expression())
         if cond_type != 'B':
@@ -192,6 +208,7 @@ class TypeChecker(PLC_ProjectVisitor):
             self.visit(ctx.statement(1))
         return None
 
+    # while
     def visitWhileStatement(self, ctx):
         cond_type = self.visit(ctx.expression())
         if cond_type != 'B':
@@ -199,6 +216,7 @@ class TypeChecker(PLC_ProjectVisitor):
         self.visit(ctx.statement())
         return None
 
+    # read
     def visitReadStatement(self, ctx):
         for id_node in ctx.idList().ID():
             name = id_node.getText()
@@ -206,6 +224,7 @@ class TypeChecker(PLC_ProjectVisitor):
                 self.report_error(id_node.getSymbol(), f"Proměnná '{name}' nebyla deklarována.")
         return None
 
+    # write
     def visitWriteStatement(self, ctx):
         for expr in ctx.exprList().expression():
             self.visit(expr)
